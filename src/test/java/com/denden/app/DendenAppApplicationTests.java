ceee6f524b56517e7829b1e71ae3e6faeae8f972
cocvu99 <cocvu99@gmail.com>
2026-02-07 17:41:45 +0700

    Init Spring Gradle project & add port config

---
+++ src/test/java/com/denden/app/DendenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.denden.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DendenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
